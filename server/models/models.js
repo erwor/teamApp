7caf9cbb2799c38c5ceadc3f47defe755625d42f
erwor <erworeduardo@gmail.com>
2017-02-16 19:11:11 -0400

    mongo

--- server/models/models.js
+++ server/models/models.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/teamapp');
+mongoose.connect('mongodb://heroku_ck8sl4p8:vpt62j2p9pkb6ppopjl86jmdf@ds153609.mlab.com:53609/heroku_ck8sl4p8');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Error de conexión!'));
